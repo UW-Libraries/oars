78cd40347535a55c7accba57ae7c6053a6e27de9
Bess Sadler <bess@curationexperts.com>
2019-12-12 18:16:34 -0500

    Add UV and iiif configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,9 +19,9 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # Enable serving of static files. Required for IIIF.
+  # See https://github.com/samvera/hyrax/wiki/Hyrax-Management-Guide
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier

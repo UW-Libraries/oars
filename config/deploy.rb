dbaad66a33dfbd8c0b872b25d2be65f1d029aa25
David Tolmie <dtolmie@uw.edu>
2020-01-16 16:18:29 -0800

    Linted

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,9 +32,9 @@ append :linked_files, '.env.production'
 # We have to re-define capistrano-sidekiq's tasks to work with
 # systemctl in production. Note that you must clear the previously-defined
 # tasks before re-defining them.
-Rake::Task["sidekiq:stop"].clear_actions
-Rake::Task["sidekiq:start"].clear_actions
-Rake::Task["sidekiq:restart"].clear_actions
+Rake::Task['sidekiq:stop'].clear_actions
+Rake::Task['sidekiq:start'].clear_actions
+Rake::Task['sidekiq:restart'].clear_actions
 namespace :sidekiq do
   task :stop do
     on roles(:app) do

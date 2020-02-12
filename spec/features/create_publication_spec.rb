29e38aa185e811d4ced433d18c024314b5154b1b
David Tolmie <dtolmie@uw.edu>
2020-02-12 11:00:14 -0800

    Added unmodified Publication work type and fixed test issues. (#43)
    * Added unmodified Publication work type and fixed test issues.
    * Added env and updated gitignore

--- spec/features/create_publication_spec.rb
+++ spec/features/create_publication_spec.rb
@@ -1,10 +1,10 @@
 # Generated via
-#  `rails generate hyrax:work Work`
+#  `rails generate hyrax:work Publication`
 require 'rails_helper'
 include Warden::Test::Helpers
 
 # NOTE: If you generated more than one work, you have to set "js: true"
-RSpec.feature 'Create a Work', js: true do
+RSpec.feature 'Create a Publication', js: true do
   context 'a logged in user' do
     let(:user_attributes) do
       { email: 'test@example.com' }
@@ -32,23 +32,22 @@ RSpec.feature 'Create a Work', js: true do
 
     scenario do
       visit '/dashboard'
-      click_link 'Works'
-      click_link 'Add new work'
-
+      click_link "Works"
+      click_link "Add new work"
       # If you generate more than one work uncomment these lines
       page.find(:css, 'h2#select-worktype-label').click
-      choose "payload_concern", option: "Work"
+      choose "payload_concern", option: "Publication"
       click_button "Create work"
 
-      expect(page).to have_content 'Add New Work'
-      click_link 'Files' # switch tab
-      expect(page).to have_content 'Add files'
-      expect(page).to have_content 'Add folder'
+      expect(page).to have_content "Add New Publication"
+      click_link "Files" # switch tab
+      expect(page).to have_content "Add files"
+      expect(page).to have_content "Add folder"
       within('span#addfiles') do
-        attach_file('files[]', "#{Hyrax::Engine.root}/spec/fixtures/image.jp2", visible: false)
-        attach_file('files[]', "#{Hyrax::Engine.root}/spec/fixtures/jp2_fits.xml", visible: false)
+        attach_file("files[]", "#{Hyrax::Engine.root}/spec/fixtures/image.jp2", visible: false)
+        attach_file("files[]", "#{Hyrax::Engine.root}/spec/fixtures/jp2_fits.xml", visible: false)
       end
-      click_link 'Descriptions' # switch tab
+      click_link "Descriptions" # switch tab
       fill_in('Title', with: 'My Test Work')
       fill_in('Creator', with: 'Doe, Jane')
       fill_in('Keyword', with: 'testing')
@@ -58,13 +57,13 @@ RSpec.feature 'Create a Work', js: true do
       # select box. Click outside the box so the next line can't find
       # its element
       find('body').click
-      choose('work_visibility_open')
+      choose('publication_visibility_open')
       expect(page).to have_content('Please note, making something visible to the world')
       check('agreement')
 
       click_on('Save')
       expect(page).to have_content('My Test Work')
-      expect(page).to have_content 'Your files are being processed by OARS in the background.'
+      expect(page).to have_content "Your files are being processed by OARS in the background."
     end
   end
 end
